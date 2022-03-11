a8383d930d8dd1ed2ac8ca02263d83c748f6fef9
yohannes soressa <ys10995@gmail.com>
2022-03-10 22:12:20 -0800

    size of varibales

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	long long int li;
 	float f;
 
-	printf("size of a char: %lu byte(s)\n", sizeof(c));
-	printf("size of an int: %lu byte(s)\n", sizeof(i));
-	printf("size of a long int: %lu byts(s)\n", sizeof(l));
-	printf("size of a long long int: %lu bytes(s)\n", sizeof(li));
-	printf("size of a float: %lu byte(s)\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byts(s)\n", sizeof(l));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(li));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 
 	return (0);
 }
