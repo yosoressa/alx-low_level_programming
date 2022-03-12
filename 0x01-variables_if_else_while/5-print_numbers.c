53ecd6203615acaeb267f5da9724922917ce4b77
yohannes soressa <ys10995@gmail.com>
2022-03-12 06:55:01 -0800

    5th task commit message

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+
+int main(void)
+
+{
+	int digit;
+
+	for (digit = 0 ; digit < 10 ; digit++)
+	{
+		printf("%ddigit", digit);
+	}
+
+	printf("\n");
+
+	return (0);
+}
