5e097aa2da1d393a2a711875041f032ff255d67a
yohannes soressa <ys10995@gmail.com>
2022-03-12 07:14:17 -0800

    commit message fot task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 48 ; j <= 57; j++)
+	{
+		putchar (j);
+	}
+	putchar ('\n');
+
+	return (0);
+}
