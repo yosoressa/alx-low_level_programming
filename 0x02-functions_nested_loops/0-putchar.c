095aaa41d87cbc321a34260f5196fdbf0a7ddd64
yohannes soressa <ys10995@gmail.com>
2022-03-15 15:24:12 -0700

    main.h commit message

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,12 @@
-#include "main.h"
-
-/**
- * main - Print the word "Holberton"
- *
- * Return: Always 0 (success/correct)
- */
-
-int main(void)
-{
-	int j;
-	char s[] = "_putchar";
-
-	for (j = 0; j < 8; j++)
-	{
-		_putchar(s[j]);
-	}
-	_putchar('\n');
-
-	return (0);
-}
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
