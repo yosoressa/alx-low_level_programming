a3dea895225a153efbe46a34bc5edf1e8f1c2161
yohannes soressa <ys10995@gmail.com>
2022-03-17 08:22:25 -0700

    commit message for task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,7 +15,8 @@ void jack_bauer(void)
 		{
 			for (d = 48; d <= 57; d++)
 			{
-				for (d = 48; d <= 57; d++){
+				for (d = 48; d <= 57; d++)
+				{
 					if (a >= 50 && b >= 52)
 					break;
 					_putchar(a);
