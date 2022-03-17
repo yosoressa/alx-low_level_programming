2616ddaa4571c0f8fc6ebd4447c17b2a94de8b4e
yohannes soressa <ys10995@gmail.com>
2022-03-17 09:07:56 -0700

    11th task of commimt message

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,11 +12,10 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		printf("%d, ", n);
-	}
-	for (; n > 98; n--)
-	{
-		printf("%d, ", n);
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
 	}
 	else if (n < 98)
 	{
