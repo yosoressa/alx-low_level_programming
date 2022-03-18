004fe15c73b81f37f545f4dce6fa87e3f3de0f39
yohannes soressa <ys10995@gmail.com>
2022-03-18 13:40:01 -0700

    commit message of task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * print_numbers - function that prints the numbers, from 0 to 9.
@@ -9,5 +9,5 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 		_putchar(c);
-	_putchar('\n');
+		_putchar('\n');
 }
