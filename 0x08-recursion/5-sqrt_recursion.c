95923390ae28c38beab5c931164a3fe00dfbc1fc
yohannes soressa <ys10995@gmail.com>
2022-03-30 13:40:05 -0700

    commit message of task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,10 +9,10 @@ int _evaluate(int i, int n)
 {
 	if (n == 0 || n == 1)
 		return (n);
-	
+
 	else if (i * i < n)
 		return (_evaluate(i + 1, n));
-	
+
 	else if (i * i == n)
 		return (i);
 
@@ -21,12 +21,19 @@ int _evaluate(int i, int n)
 	return (-1);
 }
 
+/**
+ * _sqrt_recursion - evaluate sqrt
+ * @n: interger
+ * Return: Sgrt_recursion
+ */
+
 int _sqrt_recursion(int n)
 {
 	int i = 0;
 
 	if (i < 0)
 		return (-1);
+
 	else
 		return (_evaluate(i, n));
 }
