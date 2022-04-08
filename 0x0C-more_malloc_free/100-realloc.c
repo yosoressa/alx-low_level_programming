e0f69a197d4aa20a53d349c8783e49f95ac8f0a1
yohannes soressa <ys10995@gmail.com>
2022-04-08 15:23:41 -0700

    malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,14 +27,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	else if (new_size == old_size)
 		return (ptr);
-	
 	p = malloc(new_size);
 	if (p == NULL)
 		return (NULL);
 	if (new_size > old_size)
-	{
 		max = old_size;
-	}
 	for (i = 0; i < max; i++)
 		p[i] = oldp[i];
 	free(ptr);
